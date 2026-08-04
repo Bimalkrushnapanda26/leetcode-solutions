423647ba344f4fb2792794eb76e94cd98e302e8a
Bimalkrushnapanda26 <pandabimalkrushna@gmail.com>
2026-08-04 23:57:57 +0530

    Time: 3 ms (100%), Space: 175.8 MB (15.57%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,45 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n=nums.length;
-        int positive[]=new int[n/2];
-        int negetive[]=new int[n/2];
         int pos=0;
-        int neg=0;
-        int flag=0;
+        int neg=1;
+        int result[]=new int[n];
         for(int i=0;i<n;i++)
         {
             if(nums[i]>0)
             {
-                positive[pos]=nums[i];
-                pos++;
+                result[pos]=nums[i];
+                pos+=2;
             }
             else
             {
-                negetive[neg]=nums[i];
-                neg++;
+                result[neg]=nums[i];
+                neg+=2;
             }
         }
-        // now merge the two array
-        pos=0;
-        neg=0;
-        int pointer=0;
-        while(pos<positive.length || neg<negetive.length)
-        {
-            if(flag==0)
-            {
-                nums[pointer]=positive[pos];
-                pos++;
-                pointer++;
-                flag=1;
-            }
-            else if(flag==1)
-            {
-                nums[pointer]=negetive[neg];
-                neg++;
-                pointer++;
-                flag=0;
-            }
-        }
-        return nums;
+        return result;
     }
 }
