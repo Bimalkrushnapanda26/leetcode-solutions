654479d2b78d2f3023236523210999771863bec1
Bimalkrushnapanda26 <pandabimalkrushna@gmail.com>
2026-08-11 11:18:50 +0530

    Time: 1 ms (97.42%), Space: 47.4 MB (92.21%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,73 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstrowzero=false;
+        boolean firstcolumnzero=false;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        // check that wheather there is any zeo is there or not in the first column 
+        for(int i=0;i < n;i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                firstrowzero=true;
+                break;
+            }
+        }
+        // check that wheather there is zero in the first column or not
+        for(int j=0;j < m;j++)
+        {
+            if(matrix[j][0]==0)
+            {
+                firstcolumnzero=true;
+                break;
+            }
+        }
+        // add the first row and first column as marker
+        for(int i=1;i < m;i++)
+        {
+            for(int j=1;j < n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i < m;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int k=1;k < n;k++)
+                {
+                    matrix[i][k]=0;
+                }
+            }
+        }
+        for(int i=1;i < n;i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                for(int k=1;k < m;k++)
+                {
+                    matrix[k][i]=0;
+                }
+            }
+        }
+        if(firstrowzero)
+        {
+            for(int i=0;i  <n;i++)
+            {
+                matrix[0][i]=0;
+            }
+        }
+        if(firstcolumnzero)
+        {
+            for(int i=0;i < m;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+
+    }
+}
