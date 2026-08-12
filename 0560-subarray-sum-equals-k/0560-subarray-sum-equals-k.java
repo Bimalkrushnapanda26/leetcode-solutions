5f714610aedb8e162e84710a208e0eab20a5f8c5
Bimalkrushnapanda26 <pandabimalkrushna@gmail.com>
2026-08-12 09:25:51 +0530

    Time: 21 ms (97.17%), Space: 49.1 MB (22.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,8 +1,8 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n=nums.length;
+        int ps[]=new int[n];
         int count=0;
-        int ps[]=new int[n];//it is used to store the prefix sum
         for(int i=0;i<n;i++)
         {
             if(i==0)
@@ -14,36 +14,38 @@ class Solution {
                 ps[i]=ps[i-1]+nums[i];
             }
         }
-        HashMap<Integer,Integer> prefixsum_count = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<n;i++)
         {
             if(ps[i]==k)
             {
                 count+=1;
             }
-            if(!prefixsum_count.containsKey(ps[i]-k))
+            if(!map.containsKey(ps[i]-k))
             {
-                if(prefixsum_count.containsKey(ps[i]))
+                if(map.containsKey(ps[i]))
                 {
-                    prefixsum_count.put(ps[i],prefixsum_count.get(ps[i])+1);
+                    map.put(ps[i],map.get(ps[i])+1);
                 }
                 else
                 {
-                    prefixsum_count.put(ps[i],1);
+                    map.put(ps[i],1);
                 }
             }
-            else{
-                count+=prefixsum_count.get(ps[i]-k);
-                if(prefixsum_count.containsKey(ps[i]))
+            else
+            {
+                count+=map.get(ps[i]-k);
+                if(map.containsKey(ps[i]))
                 {
-                    prefixsum_count.put(ps[i],prefixsum_count.get(ps[i])+1);
+                    map.put(ps[i],map.get(ps[i])+1);
                 }
                 else
                 {
-                    prefixsum_count.put(ps[i],1);
+                    map.put(ps[i],1);
                 }
             }
         }
         return count;
+
     }
 }
