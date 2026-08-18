3a47122460377bbd09ad93dadbc77f63dbfc81d3
Bimalkrushnapanda26 <pandabimalkrushna@gmail.com>
2026-08-18 11:59:42 +0530

    Time: 34 ms (60.11%), Space: 59.1 MB (60.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,59 +1,50 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
-        int first=0;
-        int start=0;
-        int end=n-1;
-        int temp=0;
+
         List<List<Integer>> ans = new ArrayList<>();
-        while(first<n)
-        {
-            start=first+1;
-            end=n-1;
-            while(start<end)
-            {
-                if(nums[first]+nums[start]+nums[end]==0)
-                {
-                    List<Integer> numbers = new ArrayList<>();
-                    numbers.add(nums[first]);
-                    numbers.add(nums[start]);
-                    numbers.add(nums[end]);
-                    ans.add(numbers);
-                    temp=nums[end];
-                    while(end>0 && temp==nums[end])
-                    {
-                        end--;
-                    }
-                    temp=nums[start];
-                    while(start<n && temp==nums[start])
-                    {
-                        start++;
-                    }
-                }
-                else if(nums[first]+nums[start]+nums[end]>0)
-                {
-                    temp=nums[end];
-                    while(end>0 && temp==nums[end])
-                    {
-                        end--;
-                    }
-                }
-                else
-                {
-                    temp=nums[start];
-                    while(start<n && temp==nums[start])
-                    {
-                        start++;
-                    }
-                }
+        int n = nums.length;
+
+        for (int first = 0; first < n - 2; first++) {
+
+            // Skip duplicate first elements
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
             }
-            temp=nums[first];
-            while(first<n && temp==nums[first])
-            {
-                first++;
+
+            int start = first + 1;
+            int end = n - 1;
+
+            while (start < end) {
+                int sum = nums[first] + nums[start] + nums[end];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(
+                        nums[first],
+                        nums[start],
+                        nums[end]
+                    ));
+
+                    start++;
+                    end--;
+
+                    // Skip duplicates
+                    while (start < end && nums[start] == nums[start - 1]) {
+                        start++;
+                    }
+
+                    while (start < end && nums[end] == nums[end + 1]) {
+                        end--;
+                    }
+
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
             }
         }
+
         return ans;
     }
 }
